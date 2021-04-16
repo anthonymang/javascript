306961bd617fba657574b08a9487ee566ae5efa9
anthonymang <anthonymanganielloda@gmail.com>
2021-04-16 12:08:41 -0400

    ~

---
+++ timing-functions/timing.js
@@ -0,0 +1,21 @@
+function microWave(){
+    console.log("BEEP - Food is ready")
+}
+
+let cookLength = 15000;
+setTimeout(microWave, cookLength);
+
+
+// Interval exercise - make phone ring every 2 seconds, send to voicemail after 10 rings
+function callSound() {
+    console.log("Ring!")
+}
+
+function goToVoiceMail(){
+    clearInterval(phoneCall);
+    console.log("Please leave a message after the beep.");
+}
+
+const phoneCall = setInterval(callSound, 2000);
+
+setTimeout(goToVoiceMail, 20000);
